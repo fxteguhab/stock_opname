cca1442ec56dbef7c7ad9cbd7e4513b75595e2fd
Devi Handevi <devihandevi@yahoo.com>
2017-04-21 15:52:01 +0700

    [TL-174] Generate Stock Opname from Rules

--- stock_opname.py
+++ stock_opname.py
@@ -1,6 +1,9 @@
 from openerp.osv import fields, osv
 import openerp.addons.decimal_precision as dp
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from openerp.tools.translate import _
+from datetime import datetime, timedelta
+
 
 class stock_opname_rule(osv.osv):
 	_name = "stock.opname.rule"
@@ -22,7 +25,7 @@ class stock_opname_rule(osv.osv):
 	
 	_defaults = {
 		'is_used': False,
-		'algorithm': "def generate_stock_opname_products(supplier_id):\n	return []",
+		'algorithm': "def generate_stock_opname_products():\n	return []",
 		'max_item_count': 1,
 	}
 	
@@ -33,7 +36,7 @@ class stock_opname_rule(osv.osv):
 		('const_max_item_count', 'CHECK(max_item_count >= 0)', _('Maximum item count cannot be less than zero.')),
 		('const_max_total_qty', 'CHECK(max_total_qty >= 0)', _('Maximum total quantity cannot be less than zero.')),
 	]
-
+	
 	# OVERRIDES -------------------------------------------------------------------------------------------------------------
 	
 	def create(self, cr, uid, data, context=None):
@@ -87,10 +90,10 @@ class stock_opname_inject(osv.osv):
 	_defaults = {
 		'priority': 1,
 	}
-	
-	# OVERRIDES -------------------------------------------------------------------------------------------------------------
-	
-	
+
+# OVERRIDES -------------------------------------------------------------------------------------------------------------
+
+
 # ---------------------------------------------------------------------------------------------------------------------------
 
 
@@ -100,16 +103,77 @@ class stock_opname_memory(osv.osv_memory):
 	
 	_columns = {
 		'date': fields.datetime('Date', required=True),
-		'location_id': fields.many2one('stock.location', 'Inventoried Location'),
-		# TODO Onchange: ubah location_id di line domainnya jadi yang parent_id nya location_id ini
+		'location_id': fields.many2one('stock.location', 'Inventories Location', required=True),
 		'employee_id': fields.many2one('hr.employee', 'Employee'),
-		'line_ids': fields.one2many('stock.opname.memory.line', 'stock_opname_id', 'Inventories', help="Inventory Lines."),
+		'algorithm_id': fields.many2one('stock.opname.rule', 'Rule'),
+		'line_ids': fields.one2many('stock.opname.memory.line', 'stock_opname_memory_id', 'Inventories', help="Inventory Lines."),
 	}
 	
-	# OVERRIDES -------------------------------------------------------------------------------------------------------------
+	# DEFAULTS --------------------------------------------------------------------------------------------------------------
 	
+	def _get_line_ids(self, cr, uid, ids, context=None):
+		if context is None:
+			active_algorithm_id = self._get_algorithm_id(cr, uid, ids, context)
+			rule_obj = self.pool.get('stock.opname.rule')
+			active_algorithm = rule_obj.browse(cr, uid, active_algorithm_id)
+			try:
+				exec active_algorithm.algorithm
+				# noinspection PyUnresolvedReferences
+				product_ids = generate_stock_opname_products()
+			except:
+				raise osv.except_orm(_('Generating Stock Opname Error'),
+					_('Syntax or other error(s) in the code of selected Stock Opname Rule.'))
+			line_ids = []
+			for product_id in product_ids:
+				line_ids.append({'product_id': product_id})
+			return line_ids
 	
+	def _get_algorithm_id(self, cr, uid, ids, context=None):
+		rule_obj = self.pool.get('stock.opname.rule')
+		active_algorithm_ids = rule_obj.search(cr, uid, [('is_used', '=', True)])
+		if len(active_algorithm_ids) == 0:
+			raise osv.except_orm(_('Generating Stock Opname Error'),
+				_('There is no Stock Opname Rule marked as being used.'))
+		return active_algorithm_ids
 	
+	_defaults = {
+		'line_ids': _get_line_ids,
+		'algorithm_id': _get_algorithm_id,
+	}
+	
+	# ONCHANGE --------------------------------------------------------------------------------------------------------------
+	
+	def onchange_location_id(self, cr, uid, ids):
+		return {'value': {'line_ids': []}}
+	
+	# METHOD ----------------------------------------------------------------------------------------------------------------
+	
+	def action_generate_stock_opname(self, cr, uid, ids, context=None):
+		stock_opname_obj = self.pool.get('stock.inventory')
+		stock_opname_memory_line_obj = self.pool.get('stock.opname.memory.line')
+		today = datetime.now()
+		for memory in self.browse(cr, uid, ids):
+			memory_line_id = stock_opname_memory_line_obj.browse(cr, uid, memory.line_ids.ids)
+			line_ids = []
+			for line in memory_line_id:
+				line_ids.append((0, False, {
+					'location_id': line.location_id.id,
+					'product_id': line.product_id.id,
+					'product_uom_id': line.product_uom_id.id,
+					'product_qty': line.product_qty,
+				}))
+			
+			stock_opname = {
+				'name': 'SO ' + today.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+				'state': 'confirm',
+				'date': memory.date,
+				'expiration_date': today + timedelta(hours=memory.algorithm_id.expiration_time_length),
+				'employee_id': memory.employee_id.id,
+				'location_id': memory.location_id.id,
+				'line_ids': line_ids,
+			}
+			stock_opname_obj.create(cr, uid, stock_opname, context)
+
 # ---------------------------------------------------------------------------------------------------------------------------
 
 
@@ -118,11 +182,12 @@ class stock_opname_memory_line(osv.osv_memory):
 	_description = "Stock opname memory line"
 	
 	_columns = {
-		'stock_opname_id': fields.many2one('stock.opname.memory.line', 'Stock Opname'),
+		'stock_opname_memory_id': fields.many2one('stock.opname.memory', 'Stock Opname Memory'),
 		'product_id': fields.many2one('product.product', 'Product', required=True),
-		'location_id': fields.many2one('stock.location', 'Location'),
+		'location_id': fields.many2one('stock.location', 'Location', required=True),
 		'product_uom_id': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
-		'product_qty': fields.float('Real Quantity', digits_compute=dp.get_precision('Product Unit of Measure')),
+		'product_qty': fields.float('Real Quantity', required=True,
+			digits_compute=dp.get_precision('Product Unit of Measure')),
 	}
 	
 	_defaults = {
