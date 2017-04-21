5ab6a30917d92371a756ee7284c9b161e3fc1ea9
Devi Handevi <devihandevi@yahoo.com>
2017-04-21 10:17:38 +0700

    [TL-174] Stock Opname Rule

--- stock_opname.py
+++ stock_opname.py
@@ -7,10 +7,56 @@ class stock_opname_rule(osv.osv):
 	
 	_columns = {
 		'name': fields.char('Location Name', required=True, translate=True),
+		'is_used': fields.boolean('Is Used'),
+		'algorithm': fields.text('Algorithm', required=True),
+		'expiration_time_length': fields.float('Expiration Time Length',
+			help='Validity length in hours before a stock opname expires'),
+		'max_item_count': fields.integer('Maximum Item Count', help='Maximum item type taken per stock opname'),
+		'max_total_qty': fields.integer('Maximum Total Quantity', help='Maximum total quantity per stock opname'),
+	}
+	
+	# DEFAULTS --------------------------------------------------------------------------------------------------------------
+	
+	_defaults = {
+		'is_used': False,
+		'algorithm': "def generate_stock_opname_products(supplier_id):\n	return []",
+		'max_item_count': 1,
 	}
 
 	# OVERRIDES -------------------------------------------------------------------------------------------------------------
-
+	
+	def create(self, cr, uid, data, context=None):
+		new_algorithm = super(stock_opname_rule, self).create(cr, uid, data, context)
+		algorithm_ids = self.search(cr, uid, [('id', '!=', new_algorithm)])
+		if len(algorithm_ids) == 0:
+			if data['is_used'] == False:
+				self.write(cr, uid, [new_algorithm], {'is_used': True}, context)
+		else:
+			if data['is_used'] == True:
+				for algorithm in self.browse(cr, uid, algorithm_ids):
+					if algorithm.is_used == True:
+						self.write(cr, uid, [algorithm.id], {'is_used': False}, context)
+						break
+		return new_algorithm
+	
+	def write(self, cr, uid, ids, data, context=None):
+		result = super(stock_opname_rule, self).write(cr, uid, ids, data, context)
+		if 'is_used' in data and data['is_used'] == True:
+			algorithm_ids = self.search(cr, uid, [('id', '!=', ids)])
+			for algorithm_id in algorithm_ids:
+				algorithm = self.browse(cr, uid, [algorithm_id])
+				if algorithm.is_used == True:
+					self.write(cr, uid, [algorithm_id], {'is_used': False}, context)
+		return result
+	
+	def unlink(self, cr, uid, ids, context=None):
+		result = super(stock_opname_rule, self).unlink(cr, uid, ids, context)
+		algorithm_ids = self.search(cr, uid, [])
+		if len(algorithm_ids) == 1:
+			for algorithm in self.browse(cr, uid, algorithm_ids):
+				if algorithm.is_used == False:
+					self.write(cr, uid, [algorithm.id], {'is_used': True}, context)
+		return result
 
 
 # ---------------------------------------------------------------------------------------------------------------------------
