3333c2cf19c2cb00abbeaaf49984af63c77b54d0
Devi Handevi <devihandevi@yahoo.com>
2017-04-21 10:58:14 +0700

    [TL-174] SO Rule ada constraint

--- stock_opname.py
+++ stock_opname.py
@@ -1,18 +1,21 @@
 from openerp.osv import fields, osv
 import openerp.addons.decimal_precision as dp
+from openerp.tools.translate import _
 
 class stock_opname_rule(osv.osv):
 	_name = "stock.opname.rule"
 	_description = "Stock opname rule"
 	
 	_columns = {
-		'name': fields.char('Location Name', required=True, translate=True),
+		'name': fields.char('Rule Name', required=True, translate=True),
 		'is_used': fields.boolean('Is Used'),
 		'algorithm': fields.text('Algorithm', required=True),
-		'expiration_time_length': fields.float('Expiration Time Length',
+		'expiration_time_length': fields.float('Expiration Time Length', required=True,
 			help='Validity length in hours before a stock opname expires'),
-		'max_item_count': fields.integer('Maximum Item Count', help='Maximum item type taken per stock opname'),
-		'max_total_qty': fields.integer('Maximum Total Quantity', help='Maximum total quantity per stock opname'),
+		'max_item_count': fields.integer('Maximum Item Count', required=True,
+			help='Maximum item type taken per stock opname'),
+		'max_total_qty': fields.integer('Maximum Total Quantity', required=True,
+			help='Maximum total quantity per stock opname'),
 	}
 	
 	# DEFAULTS --------------------------------------------------------------------------------------------------------------
@@ -22,6 +25,14 @@ class stock_opname_rule(osv.osv):
 		'algorithm': "def generate_stock_opname_products(supplier_id):\n	return []",
 		'max_item_count': 1,
 	}
+	
+	# CONSTRAINT ------------------------------------------------------------------------------------------------------------
+	
+	_sql_constraints = [
+		('const_expiration_time_length', 'CHECK(expiration_time_length >= 0)', _('Expiration time length cannot be less than zero.')),
+		('const_max_item_count', 'CHECK(max_item_count >= 0)', _('Maximum item count cannot be less than zero.')),
+		('const_max_total_qty', 'CHECK(max_total_qty >= 0)', _('Maximum total quantity cannot be less than zero.')),
+	]
 
 	# OVERRIDES -------------------------------------------------------------------------------------------------------------
 	
