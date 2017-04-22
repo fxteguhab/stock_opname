b140aeb92134f5abb4b35cb76ef424659cafcb86
Devi Handevi <devihandevi@yahoo.com>
2017-04-22 21:07:04 +0700

    [TL-181] Stock Opname, memory (inject+rule) dan banyak perubahan di class lainnya

--- stock_opname.py
+++ stock_opname.py
@@ -25,7 +25,7 @@ class stock_opname_rule(osv.osv):
 	
 	_defaults = {
 		'is_used': False,
-		'algorithm': "def generate_stock_opname_products():\n	return []",
+		'algorithm': "def generate_stock_opname_products():\n	return [{'product_id':1, 'product_qty':10}]",
 		'max_item_count': 1,
 	}
 	
@@ -40,36 +40,36 @@ class stock_opname_rule(osv.osv):
 	# OVERRIDES -------------------------------------------------------------------------------------------------------------
 	
 	def create(self, cr, uid, data, context=None):
-		new_algorithm = super(stock_opname_rule, self).create(cr, uid, data, context)
-		algorithm_ids = self.search(cr, uid, [('id', '!=', new_algorithm)])
-		if len(algorithm_ids) == 0:
+		new_rule = super(stock_opname_rule, self).create(cr, uid, data, context)
+		rule_ids = self.search(cr, uid, [('id', '!=', new_rule)])
+		if len(rule_ids) == 0:
 			if data['is_used'] == False:
-				self.write(cr, uid, [new_algorithm], {'is_used': True}, context)
+				self.write(cr, uid, [new_rule], {'is_used': True}, context)
 		else:
 			if data['is_used'] == True:
-				for algorithm in self.browse(cr, uid, algorithm_ids):
-					if algorithm.is_used == True:
-						self.write(cr, uid, [algorithm.id], {'is_used': False}, context)
+				for rule in self.browse(cr, uid, rule_ids):
+					if rule.is_used == True:
+						self.write(cr, uid, [rule.id], {'is_used': False}, context)
 						break
-		return new_algorithm
+		return new_rule
 	
 	def write(self, cr, uid, ids, data, context=None):
 		result = super(stock_opname_rule, self).write(cr, uid, ids, data, context)
 		if 'is_used' in data and data['is_used'] == True:
-			algorithm_ids = self.search(cr, uid, [('id', '!=', ids)])
-			for algorithm_id in algorithm_ids:
-				algorithm = self.browse(cr, uid, [algorithm_id])
-				if algorithm.is_used == True:
-					self.write(cr, uid, [algorithm_id], {'is_used': False}, context)
+			rule_ids = self.search(cr, uid, [('id', '!=', ids)])
+			for rule_id in rule_ids:
+				rule = self.browse(cr, uid, [rule_id])
+				if rule.is_used == True:
+					self.write(cr, uid, [rule_id], {'is_used': False}, context)
 		return result
 	
 	def unlink(self, cr, uid, ids, context=None):
 		result = super(stock_opname_rule, self).unlink(cr, uid, ids, context)
-		algorithm_ids = self.search(cr, uid, [])
-		if len(algorithm_ids) == 1:
-			for algorithm in self.browse(cr, uid, algorithm_ids):
-				if algorithm.is_used == False:
-					self.write(cr, uid, [algorithm.id], {'is_used': True}, context)
+		rule_ids = self.search(cr, uid, [])
+		if len(rule_ids) == 1:
+			for rule in self.browse(cr, uid, rule_ids):
+				if rule.is_used == False:
+					self.write(cr, uid, [rule.id], {'is_used': True}, context)
 		return result
 
 
@@ -82,6 +82,8 @@ class stock_opname_inject(osv.osv):
 	
 	_columns = {
 		'product_id': fields.many2one('product.product', 'Product', required=True),
+		'product_qty': fields.float('Product Quantity', required=True,
+			digits_compute=dp.get_precision('Product Unit of Measure')),
 		'priority': fields.selection([(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6')], 'Priority',
 			required=True),
 	}
@@ -105,7 +107,7 @@ class stock_opname_memory(osv.osv_memory):
 		'date': fields.datetime('Date', required=True),
 		'location_id': fields.many2one('stock.location', 'Inventoried Location', required=True),
 		'employee_id': fields.many2one('hr.employee', 'Employee'),
-		'algorithm_id': fields.many2one('stock.opname.rule', 'Rule'),
+		'rule_id': fields.many2one('stock.opname.rule', 'Rule'),
 		'line_ids': fields.one2many('stock.opname.memory.line', 'stock_opname_memory_id', 'Inventories', help="Inventory Lines."),
 	}
 	
@@ -113,32 +115,70 @@ class stock_opname_memory(osv.osv_memory):
 	
 	def _get_line_ids(self, cr, uid, context=None):
 		if context is None or (context is not None and not context.get('is_override', False)):
-			active_algorithm_id = self._get_algorithm_id(cr, uid, context)
+		# Getting the rule first
+			active_rule_id = self._get_rule_id(cr, uid, context)
 			rule_obj = self.pool.get('stock.opname.rule')
-			active_algorithm = rule_obj.browse(cr, uid, active_algorithm_id)
+			active_rule = rule_obj.browse(cr, uid, active_rule_id)
 			try:
-				exec active_algorithm.algorithm
+				exec active_rule.algorithm
 				# noinspection PyUnresolvedReferences
-				product_ids = generate_stock_opname_products()
+				products_from_rule = generate_stock_opname_products()
 			except:
 				raise osv.except_orm(_('Generating Stock Opname Error'),
 					_('Syntax or other error(s) in the code of selected Stock Opname Rule.'))
+			
+		# Process the rule with product_ids in inject if any
 			line_ids = []
-			for product_id in product_ids:
-				line_ids.append({'product_id': product_id})
+			product_ids_taken = []
+			maximum_item_count = active_rule.max_item_count
+			total_qty = 0
+			maximum_qty = active_rule.max_total_qty
+			
+			line_ids_from_inject = []
+			stock_opname_inject_obj = self.pool.get('stock.opname.inject')
+			product_ids_from_inject = stock_opname_inject_obj.search(cr, uid, [], order='priority ASC')
+			for product_id in product_ids_from_inject:
+				product_inject = stock_opname_inject_obj.browse(cr, uid, product_id)
+				if total_qty+product_inject.product_qty <= maximum_qty and \
+				product_inject.product_id not in product_ids_taken and len(product_ids_taken)+1 <= maximum_item_count:
+					total_qty += product_inject.product_qty
+					product_ids_taken.append(product_inject.product_id)
+					line_ids_from_inject.append({'product_id': product_inject.product_id,
+												'product_qty': product_inject.product_qty,
+												'is_inject': True, })
+				elif total_qty == maximum_qty or len(product_ids_taken) == maximum_item_count:
+					break
+			line_ids.extend(line_ids_from_inject)
+			
+		# Process the rule with the algorithm
+			line_ids_from_rule = []
+			for product in products_from_rule:
+				if total_qty+product['product_qty'] <= maximum_qty and \
+				product['product_id'] not in product_ids_taken and len(product_ids_taken)+1 <= maximum_item_count:
+					total_qty += product['product_qty']
+					product_ids_taken.append(product['product_id'])
+					line_ids_from_rule.append({'product_id': product['product_id'],
+												'product_qty': product['product_qty'],
+												'is_inject': False, })
+				elif total_qty == maximum_qty or len(product_ids_taken) == maximum_item_count:
+					break
+			line_ids.extend(line_ids_from_rule)
 			return line_ids
 	
-	def _get_algorithm_id(self, cr, uid, context=None):
-		rule_obj = self.pool.get('stock.opname.rule')
-		active_algorithm_ids = rule_obj.search(cr, uid, [('is_used', '=', True)])
-		if len(active_algorithm_ids) == 0:
-			raise osv.except_orm(_('Generating Stock Opname Error'),
-				_('There is no Stock Opname Rule marked as being used.'))
-		return active_algorithm_ids[0]
+	def _get_rule_id(self, cr, uid, context=None):
+		if context is None or (context is not None and not context.get('is_override', False)):
+			rule_obj = self.pool.get('stock.opname.rule')
+			active_rule_ids = rule_obj.search(cr, uid, [('is_used', '=', True)])
+			if len(active_rule_ids) == 0:
+				raise osv.except_orm(_('Generating Stock Opname Error'),
+					_('There is no Stock Opname Rule marked as being used.'))
+			return active_rule_ids[0]
+		else:
+			return 0
 	
 	_defaults = {
+		'rule_id': _get_rule_id,
 		'line_ids': _get_line_ids,
-		'algorithm_id': _get_algorithm_id,
 	}
 	
 	# ONCHANGE --------------------------------------------------------------------------------------------------------------
@@ -161,11 +201,12 @@ class stock_opname_memory(osv.osv_memory):
 					'product_id': line.product_id.id,
 					'product_uom_id': line.product_uom_id.id,
 					'product_qty': line.product_qty,
+					'is_inject': line.is_inject,
 				}))
 				
 			stock_opname = {
 				'name': 'SO ' + today.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
-				'state': 'confirm',
+				'state': 'confirm' if memory.algorithm_id.id else 'done',
 				'date': memory.date,
 				'expiration_date': (datetime.strptime(memory.date, DEFAULT_SERVER_DATETIME_FORMAT)
 									+ timedelta(hours=memory.algorithm_id.expiration_time_length))
@@ -190,6 +231,7 @@ class stock_opname_memory_line(osv.osv_memory):
 		'product_uom_id': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
 		'product_qty': fields.float('Real Quantity', required=True,
 			digits_compute=dp.get_precision('Product Unit of Measure')),
+		'is_inject': fields.boolean('Is Inject?'),
 	}
 	
 	_defaults = {
