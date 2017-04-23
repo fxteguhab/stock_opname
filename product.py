2824dad6853bd5373686659edda83df22e8b0574
Devi Handevi <devihandevi@yahoo.com>
2017-04-23 19:40:40 +0700

    [TL-189] Implementasi Algo ok, belum ada last_sale function yg udah jalan bener

--- product.py
+++ product.py
@@ -1,4 +1,5 @@
 from openerp.osv import osv, fields
+from datetime import datetime, timedelta
 
 # ==========================================================================================================================
 
@@ -6,11 +7,18 @@ class product_product(osv.osv):
 	
 	_inherit = 'product.product'
 	
+	# METHODS ---------------------------------------------------------------------------------------------------------------
+	
+	def _get_last_sale(self, cr, uid):
+		return None
+	
 	# COLUMNS ---------------------------------------------------------------------------------------------------------------
 	
 	_columns = {
 		'latest_inventory_adjustment_date': fields.datetime('Latest Inventory Adjustment Date'),
-		'latest_inventory_adjustment_employee': fields.many2one('hr.employee', 'Latest Inventory Adjustment Employee'),
+		'latest_inventory_adjustment_employee_id': fields.many2one('hr.employee', 'Latest Inventory Adjustment Employee'),
+		'last_sale': fields.function(_get_last_sale, string="Last Sale", type='datetime', readonly=True, store={}),
 	}
-
+	
+	
 # ==========================================================================================================================
