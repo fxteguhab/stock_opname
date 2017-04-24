2efc5a9aec3884cbda5ba256867a25f10526cd35
Devi Handevi <devihandevi@yahoo.com>
2017-04-24 10:09:17 +0700

    [TS-206] _get_last_sale

--- product.py
+++ product.py
@@ -1,5 +1,6 @@
 from openerp.osv import osv, fields
 from datetime import datetime, timedelta
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 # ==========================================================================================================================
 
@@ -10,7 +11,16 @@ class product_template(osv.osv):
 	# METHODS ---------------------------------------------------------------------------------------------------------------
 	
 	def _get_last_sale(self, cr, uid, ids, name, args, context=None):
-		return None
+		sale_order_obj = self.pool.get('sale.order')
+		sale_order_line_obj = self.pool.get('sale.order.line')
+		sale_order_line_ids = sale_order_line_obj.search(cr, uid, [('product_id', 'in', ids)])
+		sale_order_ids = []
+		for sale_order_line_id in sale_order_line_ids:
+			sale_order_line = sale_order_obj.browse(cr, uid, sale_order_line_id)
+			sale_order_ids.append(sale_order_line.order_id)
+		ordered_sale_order_ids = sale_order_obj.search(cr, uid, sale_order_ids, order='date_order DESC')
+		last_sale_order = sale_order_obj.browse(cr, uid, ordered_sale_order_ids[0])
+		return datetime.strptime(last_sale_order.date_order, DEFAULT_SERVER_DATETIME_FORMAT)
 	
 	# COLUMNS ---------------------------------------------------------------------------------------------------------------
 	
